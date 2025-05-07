0dc1b4742ca153a4fdfc13828cf186ce3afaf07a
essentials-s <esbergenerdaulet777@gmail.com>
2025-05-07 19:56:54 +0500

    Update gaxball.js

--- gaxball.js
+++ gaxball.js
@@ -902,3 +902,29 @@ function filterByCountry(rooms, countryCode) {
 }
 
 const filteredRooms = filterByCountry(roomList, "RU"); // Только комнаты из России
+
+var room = HBInit({
+	roomName: "A t l a s  S e r v e r",
+	maxPlayers: 16,
+	noPlayer: true // Remove host player (recommended!)
+});
+room.setDefaultStadium("Big");
+room.setScoreLimit(5);
+room.setTimeLimit(0);
+
+// If there are no admins left in the room give admin to one of the remaining players.
+function updateAdmins() { 
+  // Get all players
+  var players = room.getPlayerList();
+  if ( players.length == 0 ) return; // No players left, do nothing.
+  if ( players.find((player) => player.admin) != null ) return; // There's an admin left so do nothing.
+  room.setPlayerAdmin(players[0].id, true); // Give admin to the first non admin player in the list
+}
+
+room.onPlayerJoin = function(player) {
+  updateAdmins();
+}
+
+room.onPlayerLeave = function(player) {
+  updateAdmins();
+}
